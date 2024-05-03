49a2137f526e932bf065c7c0970c66ae9cb6a7c3
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-03 17:36:37 +0530

    Update and rename CREATE DATABASE to CREATE DATABASE.sql

---
+++ CREATE DATABASE.sql
@@ -0,0 +1,11 @@
+//to create a database named college 
+mysql> CREATE DATABASE COLLEGE;
+Query OK, 1 row affected (0.02 sec)
+
+//to select this database college to use further
+mysql> USE COLLEGE;
+Database changed
+
+//to delete the existing database college
+mysql> DROP DATABASE COLLEGE;
+Query OK, 0 rows affected (0.08 sec)
